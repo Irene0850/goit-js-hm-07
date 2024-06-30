873b0ba037c378278db5dca12dad1da6ab8a2dfc
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-06-30 12:15:46 +0300

    new

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,33 @@
+const formEl = document.querySelector(".login-form");
+const labelsEl = document.querySelectorAll(".login-form label");
+const inputEmailEl = document.querySelector('[name = "email"]');
+const inputPasswordEl = document.querySelector('[name = "password"]');
+const buttonEl = document.querySelector(".login-form button");
 
+labelsEl.forEach((label) => {
+  label.classList.add("label-js");
+});
+
+buttonEl.classList.add("btn-js");
+
+inputEmailEl.style.height = "40px";
+inputPasswordEl.style.height = "40px";
+
+formEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailValue = inputEmailEl.ariaValueMax.trim();
+  const passwordValue = inputPasswordEl.ariaValueMax.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const formData = {
+      email: emailValue,
+      password: passwordValue,
+    };
+    console.log(formData);
+
+    buttonEl.form.reset();
+  }
+});
