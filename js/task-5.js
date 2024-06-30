24c9d3d3698d87637e2567dfe08e6063e6184a1d
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-06-30 12:37:53 +0300

    new

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,17 @@
+const buttonForChangeColor = document.querySelector(".change-color");
+const descriptionColor = document.querySelector(".color");
+const body = document.querySelector("body");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+buttonForChangeColor.addEventListener("click", function (event) {
+  event.preventDefault();
+  const colorForBody = getRandomHexColor();
+  body.style.backgroundColor = colorForBody;
+
+  descriptionColor.textContent = colorForBody;
+});
