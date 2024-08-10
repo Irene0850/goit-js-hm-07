239faf70f0139857d9db760f15240b14e0afaf28
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-08-10 15:34:16 +0300

    new

--- js/task-6.js
+++ js/task-6.js
@@ -7,31 +7,39 @@ const refs = {
 
 refs.buttonCreate.addEventListener("click", onCreateClick);
 refs.buttonDestroy.addEventListener("click", onDestroyClick);
-function onCreateClick() {
-  const amount = refs.inputQuantity.value;
-  const value = Number(amount);
 
-  if (amount < 1 || amount > 100) {
+function onCreateClick() {
+  const amount = Number(refs.inputQuantity.value);
+  if (amount >= 1 || amount <= 100) {
+    createBoxes(amount);
+    refs.inputQuantity.value = "";
+  } else {
     return alert(
       "Ooops, something go wrong!!! The value must be less than 100"
     );
   }
-  onDestroyClick();
-  createBoxes(amount);
 }
-function onDestroyClick() {
-  refs.containerForEl.innerHTML = "";
-  refs.inputQuantity.value = "";
-}
+
 function createBoxes(amount) {
+  onDestroyClick();
+  const block = document.createDocumentFragment();
+
   for (let i = 0; i < amount; i++) {
     let elem = document.createElement("div");
     elem.style.width = 30 + 10 * i + "px";
     elem.style.height = 30 + 10 * i + "px";
     elem.style.backgroundColor = getRandomHexColor();
-    refs.containerForEl.append(elem);
+    block.appendChild(elem);
+  }
+  refs.containerForEl.appendChild(block);
+}
+
+function onDestroyClick() {
+  while (refs.containerForEl.firstChild) {
+    refs.containerForEl.removeChild(refs.containerForEl.firstChild);
   }
 }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
